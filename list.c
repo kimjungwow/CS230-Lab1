d6c8b52dcbf4e1693400a33ed44d044aa77643a4
cs20140681 <jw950310@kaist.ac.kr>
2018-08-30 21:01:11 +0900

    This is my first commit

--- list.c
+++ list.c
@@ -10,7 +10,13 @@ initialize the pointers to NULL,
 set the key value to the key provided by the argument
  */
 struct list_node *allocate_node_with_key(int key)
-{
+{ struct list_node made;
+made.key=key;
+made.next=NULL;
+made.prev=NULL;
+made.value=key;
+struct list_node *answer=&made;
+return answer;
 }
 
 /*	
