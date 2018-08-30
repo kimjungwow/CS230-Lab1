8d89c16648de02c60591281e5eba7f08d446fcdb
cs20140681 <jw950310@kaist.ac.kr>
2018-08-30 23:56:06 +0900

    17

--- list.c
+++ list.c
@@ -10,7 +10,7 @@ initialize the pointers to NULL,
 set the key value to the key provided by the argument
  */
 struct list_node *allocate_node_with_key(int key)
-{ struct list_node *made=malloc(sizeof(int));
+{ struct list_node *made=(void *)malloc(sizeof(int));
 made->key=key;
 made->next=NULL;
 made->prev=NULL;
